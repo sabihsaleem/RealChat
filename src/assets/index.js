033b4ae58ea1bab1a45f0bac20fd162ef6494cd2
Sabih Saleem <sabihsaleem1997@gmail.com>
2023-11-15 22:12:21 +0500

    development work

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+const images = {
+  splash: require('./images/Logo.png'),
+  onb: require('./images/onBoarding.png'),
+  onb1: require('./images/onBoarding1.png'),
+  onb2: require('./images/onBoarding2.png'),
+  pattern: require('./images/Pattern.png'),
+  logo: require('./images/Logo.png'),
+};
+
+const icons = {
+  splash: require('./images/Desktop.png'),
+  eyeopen: require('./images/eyeopen.png'),
+  eyeclosed: require('./images/eyeclosed.png'),
+  email: require('./images/email.png'),
+  facebook: require('./images/facebook.png'),
+  google: require('./images/google.png'),
+};
+
+export {images, icons};
