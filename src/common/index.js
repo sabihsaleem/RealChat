f0fc38b7a51477dabe786ed3337ea5c621283a25
Sabih Saleem <sabihsaleem1997@gmail.com>
2023-11-15 22:19:07 +0500

    Revert "Merge pull request #1 from sabihsaleem/development"
    This reverts commit 6cf3fc0c43a859432f3cf156a4bca0d147b4efef, reversing
    changes made to a26bb3f93f4fbe276ca6044ac3e7bf3067b12407.

--- src/common/index.js
+++
@@ -1,6 +0,0 @@
-import Fonts from './Fonts';
-import Colors from './Colors';
-import FontSizes from './FontSizes';
-import shadows from './shadows';
-
-export {Fonts, Colors, FontSizes, shadows};
--- src/screens/index.js
+++
@@ -1,7 +0,0 @@
-import Home from './Home';
-import Splash from './Splash';
-import Login from './Login';
-import Register from './Register';
-import OnBoarding from './OnBoarding';
-
-export {Home, Splash, Login, Register, OnBoarding};
