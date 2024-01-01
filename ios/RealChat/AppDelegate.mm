5cbcbe3a7f6954f6542835da0527c3c7721ab5b5
Sabih Saleem <sabih.saleem@digitrends.pk>
2024-01-01 17:13:31 +0500

    firebase added

--- ios/RealChat/AppDelegate.mm
+++ ios/RealChat/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -11,7 +12,9 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
+  [FIRApp configure];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
