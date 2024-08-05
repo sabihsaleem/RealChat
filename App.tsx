ab4e90d5dbd4fbc234f6ce70ee6caad234e20970
Sabih Saleem <sabih.saleem@metafroliclabs.com>
2024-08-05 19:04:25 +0500

    changes

--- App.tsx
+++ App.tsx
@@ -1,27 +1,172 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-import Navigation from './src/routes/Navigation';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import Toast from 'react-native-toast-message';
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
-import {store, persistor} from './src/redux/store';
+import React, {useRef, useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+  PermissionsAndroid,
+  Platform,
+} from 'react-native';
+import {
+  ClientRoleType,
+  createAgoraRtcEngine,
+  IRtcEngine,
+  RtcSurfaceView,
+  ChannelProfileType,
+} from 'react-native-agora';
+import RNCallKeep from 'react-native-callkeep';
+import styles from './styles';
+
+const appId = '8384eb7b817841228f9a84901d028722';
+const channelName = 'RealChat';
+const token =
+  '007eJxTYGC/xHP4jlvhy8jOnr2+CkxMKae08pYqsxT6bvP06lzfVK/AYGFsYZKaZJ5kYWhuYWJoZGSRZploYWJpYJhiYGRhbmQ069rBlIZARoYVhVeYGBkgEMTnYAhKTcxxzkgsYWAAAA+MHro=';
+const uid = 0;
 
 const App = () => {
+  const agoraEngineRef = useRef<IRtcEngine>(); // Agora engine instance
+  const [isJoined, setIsJoined] = useState(false); // Indicates if the local user has joined the channel
+  const [remoteUid, setRemoteUid] = useState(0); // Uid of the remote user
+  const [message, setMessage] = useState(''); // Message to the user
+
+  function showMessage(msg: string) {
+    setMessage(msg);
+  }
+
+  const getPermission = async () => {
+    if (Platform.OS === 'android') {
+      await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+      ]);
+    }
+  };
+
+  useEffect(() => {
+    // Initialize Agora engine when the app starts
+    setupVideoSDKEngine();
+  }, []);
+
+  const setupVideoSDKEngine = async () => {
+    try {
+      // use the helper function to get permissions
+      if (Platform.OS === 'android') {
+        await getPermission();
+      }
+      agoraEngineRef.current = createAgoraRtcEngine();
+      const agoraEngine = agoraEngineRef.current;
+      agoraEngine.registerEventHandler({
+        onJoinChannelSuccess: () => {
+          showMessage('Successfully joined the channel ' + channelName);
+          setIsJoined(true);
+        },
+        onUserJoined: (_connection, Uid) => {
+          showMessage('Remote user joined with uid ' + Uid);
+          setRemoteUid(Uid);
+          // Notify Caller 2 of the incoming call
+          notifyIncomingCall(Uid);
+        },
+        onUserOffline: (_connection, Uid) => {
+          showMessage('Remote user left the channel. uid: ' + Uid);
+          setRemoteUid(0);
+        },
+      });
+      agoraEngine.initialize({
+        appId: appId,
+        channelProfile: ChannelProfileType.ChannelProfileLiveBroadcasting,
+      });
+      agoraEngine.enableVideo();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const notifyIncomingCall = Uid => {
+    const callUUID = 'uniqueId'; // Generate or use a unique ID for the call
+    const handle = 'CallerId'; // Use the caller's identifier
+
+    RNCallKeep.displayIncomingCall(
+      callUUID,
+      handle,
+      `Incoming call from ${Uid}`,
+    );
+  };
+
+  const join = async () => {
+    if (isJoined) {
+      return;
+    }
+    try {
+      agoraEngineRef.current?.setChannelProfile(
+        ChannelProfileType.ChannelProfileCommunication,
+      );
+      agoraEngineRef.current?.startPreview();
+      agoraEngineRef.current?.joinChannel(token, channelName, uid, {
+        clientRoleType: ClientRoleType.ClientRoleBroadcaster,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const leave = () => {
+    try {
+      agoraEngineRef.current?.leaveChannel();
+      setRemoteUid(0);
+      setIsJoined(false);
+      showMessage('You left the channel');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const switchCamera = () => {
+    agoraEngineRef.current?.switchCamera();
+  };
+
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Navigation />
-          <Toast
-            position="bottom"
-            bottomOffset={20}
-            autoHide={true}
-            visibilityTime={5000}
-          />
-        </PersistGate>
-      </Provider>
-    </GestureHandlerRootView>
+    <SafeAreaView style={styles.main}>
+      <Text style={styles.head}>Agora Video Calling Quickstart</Text>
+      <View style={styles.btnContainer}>
+        <Text onPress={join} style={styles.button}>
+          Join
+        </Text>
+        {isJoined && remoteUid !== 0 && (
+          <>
+            <Text onPress={leave} style={styles.button}>
+              Leave
+            </Text>
+            <Text onPress={switchCamera} style={styles.button}>
+              Switch Camera
+            </Text>
+          </>
+        )}
+      </View>
+      <ScrollView
+        style={styles.scroll}
+        contentContainerStyle={styles.scrollContainer}>
+        {/* {isJoined ? (
+          <React.Fragment key={0}>
+            <RtcSurfaceView canvas={{uid: 0}} style={styles.videoView} />
+            <Text>Local user uid: {uid}</Text>
+          </React.Fragment>
+        ) : (
+          <Text>Join a channel</Text>
+        )} */}
+        {isJoined && remoteUid !== 0 ? (
+          <React.Fragment key={remoteUid}>
+            <RtcSurfaceView
+              canvas={{uid: remoteUid}}
+              style={styles.videoView}
+            />
+            <Text>Remote user uid: {remoteUid}</Text>
+          </React.Fragment>
+        ) : (
+          <Text>Waiting for a remote user to join</Text>
+        )}
+        <Text style={styles.info}>{message}</Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
